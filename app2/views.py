299aa216370dc018092bb112cd426e9e9b3c5dae
RambabuKarri <karrirambabu66@gmail.com>
2023-05-24 13:53:12 +0530

    first commit

---
+++ app2/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def app2_string(request):
+    return HttpResponse('Hi this is app2_string')
+
+def app2_htmlpage(request):
+    return render(request,'app2_htmlpage.html')
